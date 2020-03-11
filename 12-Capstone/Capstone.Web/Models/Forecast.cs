b89d3ca0cce4a66c276d0b1bb073b88cb8cdd00c
Karina Gentile <karinagentile381@gmail.com>
2020-03-11 12:31:13 -0400

    added separate forecast model

---
+++ 12-Capstone/Capstone.Web/Models/Forecast.cs
@@ -0,0 +1,66 @@
+﻿using Capstone.Web.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Web.Models
+{
+    public class Forecast
+    {
+        public string Id { get; set; }
+        public bool IsFahrenheit { get; set; }
+
+            public IWeatherDAO dao;
+
+            public void GetFiveDay(IWeatherDAO dao, string id)
+            {
+                this.dao = dao;
+                this.Id = id;
+                fiveDayForecast = dao.GetForecast(id);
+            }
+
+            List<Weather> fiveDayForecast;
+
+            public List<Weather> GetForecast(string id)
+            {
+                List<Weather> list = dao.GetForecast(id);
+                return list;
+            }
+
+            public string GetParkName(string id)
+            {
+                string parkName = fiveDayForecast[0].ParkName;
+                return parkName;
+            }
+
+
+            public int FiveDayForecastValue;
+            public string GetDay()
+            {
+                if (FiveDayForecastValue == 1)
+                {
+                    return "Today";
+                }
+                else if (FiveDayForecastValue == 2)
+                {
+                    return DateTime.Today.AddDays(1).DayOfWeek.ToString();
+                }
+                else if (FiveDayForecastValue == 3)
+                {
+                    return DateTime.Today.AddDays(2).DayOfWeek.ToString();
+                }
+                else if (FiveDayForecastValue == 4)
+                {
+                    return DateTime.Today.AddDays(3).DayOfWeek.ToString();
+                }
+                else
+                {
+                    return DateTime.Today.AddDays(4).DayOfWeek.ToString();
+                }
+
+
+            }
+        }
+    }
+
--- 12-Capstone/Capstone.Web/Models/Weather.cs
+++ 12-Capstone/Capstone.Web/Models/Weather.cs
@@ -14,8 +14,7 @@ namespace Capstone.Web.Models
         public decimal Low { get; set; }
         public decimal High { get; set; }
         public string Forecast { get; set; }
-        public string Id { get; set; }
-        public bool IsFahrenheit { get; set; }
+
 
         public int CelsiusConversion(decimal temp)
         {
@@ -23,61 +22,6 @@ namespace Capstone.Web.Models
             return (int)tempInCelsius;
         }
 
-        public class FiveDayForecast
-        {
-            internal IWeatherDAO dao;
-
-            //internal void GetFiveDay(IWeatherDAO dao, string id)
-            //{
-            //    this.dao = dao;
-            //    this.Id = id;
-            //    fiveDayForecast = dao.GetForecast(id);         
-            //}
-
-            List<Weather> fiveDayForecast;
-
-
-
-            public List<Weather> GetForecast(string id)
-            {
-                List<Weather> list = dao.GetForecast(id);
-                return list;
-            }
-
-            public string GetParkName(string id)
-            {
-                string parkName = fiveDayForecast[0].ParkName;
-                return parkName;
-            }
-
-
-            public int FiveDayForecastValue;
-            public string GetDay()
-            {
-                if (FiveDayForecastValue == 1)
-                {
-                    return "Today";
-                }
-                else if (FiveDayForecastValue == 2)
-                {
-                    return DateTime.Today.AddDays(1).DayOfWeek.ToString();
-                }
-                else if (FiveDayForecastValue == 3)
-                {
-                    return DateTime.Today.AddDays(2).DayOfWeek.ToString();
-                }
-                else if (FiveDayForecastValue == 4)
-                {
-                    return DateTime.Today.AddDays(3).DayOfWeek.ToString();
-                }
-                else
-                {
-                    return DateTime.Today.AddDays(4).DayOfWeek.ToString();
-                }
-
-
-            }
-        }
     }
 }
 
