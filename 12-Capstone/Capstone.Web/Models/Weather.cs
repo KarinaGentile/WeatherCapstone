2df6d1fe12adf591c1c72238ecc9f3a3cf4d8340
Karina Gentile <karinagentile381@gmail.com>
2020-03-11 12:45:36 -0400

    added user recomendation for forecast

--- 12-Capstone/Capstone.Web/Models/Weather.cs
+++ 12-Capstone/Capstone.Web/Models/Weather.cs
@@ -22,6 +22,48 @@ namespace Capstone.Web.Models
             return (int)tempInCelsius;
         }
 
+        public string Recommendation()
+        {
+            string a = "";
+            string b = "";
+            string c = "";
+            string d = "";
+
+            if (Forecast == "snow")
+            {
+                a = "Pack snowshoes!";
+            }
+            else if (Forecast == "rain")
+            {
+                a = "Pack rain gear and wear waterproof shoes!";
+            }
+            else if (Forecast == "thunderstorms")
+            {
+                a = "Seek shelter and avoid hiking on exposed ridges!";
+            }
+            else if (Forecast == "sunny")
+            {
+                a = "Pack sunblock!";
+            }
+            else if (Forecast == "partly cloudy")
+            {
+                a = "Dress in layers!";
+            }
+            if (High > 75)
+            {
+                b = " Don't forget to bring an extra gallon of water!";
+            }
+            if (High - Low > 20)
+            {
+                c = " Be sure to wear breathable layers!";
+            }
+            if (Low < 20)
+            {
+                d = " Be careful! Exposure to frigid temperatures could be dangerous to your health!";
+            }
+            return a + b + c + d;
+        }
+
     }
 }
 
